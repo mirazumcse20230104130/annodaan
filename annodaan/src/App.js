27fa95c75378fb80b589ea6f96e143f43440a286
Rifa Rafia <tarinrifa@gmail.com>
2025-09-06 23:32:38 +0600

    added AboutUs page,create account page,aboutus made changes in navigation.js

--- annodaan/src/App.js
+++ annodaan/src/App.js
@@ -1,20 +1,25 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import Navigation from "./Navigation";
 import Header from "./Header";
-import Breadcrumbs from "./Breadcrumbs";
+import CreateAccount from "./CreateAccount";
 import MainContent from "./MainContent";
 import DonateHere from "./DonateHere";
+import AboutUs from "./AboutUs";
 
 function App() {
   return (
     <Router>
       <Header />
-      <Breadcrumbs />
-      <Routes>
-        <Route path="/" element={<MainContent />} />
-        <Route path="/donate" element={<DonateHere />} />
-      </Routes>
+      <Navigation />
+      <div className="app-content">
+        <Routes>
+          <Route path="/create-account" element={<CreateAccount />} />
+          <Route path="/" element={<MainContent />} />
+          <Route path="/donate" element={<DonateHere />} />
+          <Route path="/about" element={<AboutUs />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
--- annodaan/src/Header.js
+++ annodaan/src/Header.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import logo from './images/logo.png'; // Ensure this image exists
+import logo from './images/logo.png'; // Verify this path
 
 const Header = () => (
   <header className="header-bg">
-    <img src={logo} alt="Logo" className="logo-img me-2" />
+    <img src={logo} alt="Logo" className="logo-img me-2" onError={(e) => { e.target.style.display = 'none'; console.log('Image failed to load'); }} />
     <div className="header-content">
       <h1>Annodaan</h1>
       <p>From surplus to support!</p>
